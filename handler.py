72ebef45d9610fbf20b2929260dda0752aa42c19
Aliabbas Merchant <merchantaliabbas@gmail.com>
2021-03-17 18:51:38 +0530

    Mouse and Keyboard

--- handler.py
+++ handler.py
@@ -1,7 +1,22 @@
 from typing import Optional
 import pyautogui
+import json
 
+pyautogui.FAILSAFE = False
 
 def handle(request: dict) -> Optional[str]:
-    pyautogui.press(request['signal'])
+    print(request['signal'], request['action'])
+    if request['action'] == 'type':
+        pyautogui.press(request['signal'])
+    elif request['action'] == 'mouse':
+        signal = json.loads(request['signal'])
+        x = signal['x']
+        y = signal['y']
+        w = signal['w']
+        h = signal['h']
+        W, H = pyautogui.size()
+        x = x * W / w
+        y = y * H / h
+        print(x, y)
+        pyautogui.moveTo(x, y)
     return None
